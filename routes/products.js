3c37af3c924ff1d2dd967211e61528378ef17ce9
Şule İlkbahar <suleilkbahar@Sule-MacBook-Air.local>
2024-07-10 01:43:15 +0300

    add product, update product and delete product according to product id  are  done

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,104 @@
 var express = require('express');
-var router = express.router;
+const verifyAuth = require('./auth');
+var router = express.Router();
+const db = require('../db/knex.db');
 
- router.get('/')
+router.post('/add', verifyAuth, async function (req, res, next) {
+    const { product_name } = req.body;
+    const email = req.user.email;
+
+    try {
+        const user = await db('users').select('*').where('email', email);
+        if (!user) {
+            return res.status(400).send('There is no such a user');
+        }
+
+        if (user.user_type === 'user') { // COMPANY STRİNG OLARAK ALDIM. DİĞER TÜRLÜ HATA VERİYOR AMA USER_TYPE KISMINI ENUM OLARAK NASIL ATABİLİRİZ?
+            return res.status(400).send({
+                message: 'Users cannot add product',
+                user
+            });
+        }
+
+        const product = await db('products').insert({ product_name });
+
+        return res.status(200).send({
+            message: 'product has been added successfully',
+            product: product[0]
+        });
+
+    } catch (error) {
+        res.status(400).send('There is an error')
+    }
+});
+
+router.patch('/update/:id', verifyAuth, async function (req, res, next) {
+    const { product_name } = req.body;
+    const email = req.user.email;
+    const id = req.params.id;
+
+    try {
+        const user = await db('users').select('*').where('email', email).first();
+        if (!user) {
+            return res.status(400).send('There is no such a user');
+        }
+
+        if (user.user_type === 'user') { // COMPANY STRİNG OLARAK ALDIM. DİĞER TÜRLÜ HATA VERİYOR AMA USER_TYPE KISMINI ENUM OLARAK NASIL ATABİLİRİZ?
+            return res.status(400).send({
+                message: 'Users cannot update product',
+                user
+            });
+        }
+
+        const product = await db('products').select('*').where('id', id).first();
+        if (!product) {
+            return res.status(400).send('there is no product with this given id');
+        }
+        const updated_product = await db('products').select('*').where('id', id).update({ product_name });
+
+        return res.status(200).send({
+            message: 'product has been updated successfully',
+            product: product[0]
+        });
+
+    } catch (error) {
+        res.status(400).send('There is an error')
+    }
+});
+
+router.delete('/delete/:id', verifyAuth, async (req, res, next) => {
+    const product_id = req.params.id;
+    const email = req.user.email;
+
+    try {
+        const product = db('products').select('*').where('id', product_id).first();
+        const user = db('users').select('*').where('email', email).first();
+        if (!product) {
+            return res.status(400).send({
+                message: 'There is no product with the given id'
+            });
+        }
+        if (!user) {//gerekli mi bilemiyorum ama yazmakta fayda var
+            return res.status(400).send({
+                message: 'There is no user with the given id'
+            });
+        }
+
+        if (user.user_type === 'user') {
+            return res.status(400).send('user cannot delete a product.')
+        }
+
+        await db('products').where('id', product_id).del();
+
+        return res.status(200).send({
+            message: 'Product has been deleted successfully'
+        });
+    } catch (error) {
+        return res.status(500).send({
+            message: 'An error occurred while deleting the product'
+        });
+    }
+});
+
+
+module.exports = router;
