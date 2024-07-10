e4bec73e3f10a4793633b2607d857326e589eb3d
Şule İlkbahar <suleilkbahar@Sule-MacBook-Air.local>
2024-07-11 00:28:29 +0300

    all cart funtions  are done and also add and delete is updated

--- routes/cart.js
+++ routes/cart.js
@@ -2,63 +2,133 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db/knex.db');
 const verifyAuth = require('./auth');
-const { route } = require('./products');
 
-/*user can create cart*/
-router.post('/add', verifyAuth, async function (req, res, next) {
+router.post('/add', verifyAuth, async (req, res) => {
     const user_email = req.user.email;
-    console.log('user_email:', user_email);
-
-
+    const { id } = req.body;
 
     try {
-        //check auth
         const user = await db('users').select('*').where('email', user_email).first();
+
         if (!user) {
-            return res.status(400).send({
-                message: 'You should be authorize first.'
-            });
+            return res.status(400).send({ message: 'there is no such a user' });
         }
 
-        //chech user_type
+
         if (user.user_type !== 'user') {
-            return res.status(400).send({
-                message: 'Only the users can create cart and add produts to this cart.'
-            })
+            return res.status(400).send({ message: ' only users can add porduct to card' });
         }
 
-        // cheach if the user created cart befare
-        const cart = await db('cart').innerJoin('users', 'users.id', 'cart.user_id').select('*').where('email', user_email);
-        if (cart.length !== 0) {
-             return res.status(400).send({
-                message: 'user already has a  cart.',
-                cart
-            })
-        }
 
-        //a create cart
-        const newCart = await db('cart').insert({ user_id: user.id });
-        return res.status(200).send({
-            message: 'cart added succeffuly',
-            newCart
-        })
+        const cartId = await db('cart').insert({ user_id: user.id, product_ids: id });
+
+        return res.status(200).send({ message: 'The product added to the cart successfully' });
+
     } catch (error) {
-        res.status(400).send({
-            message: 'Something is wrong to add cart.'
-        })
-
+        console.error('Hata:', error);
+        return res.status(400).send({ message: 'Something wrong brother.' });
     }
 });
 
 
-
-router.post('/addproduct', verifyAuth,);
-
-router.delete('/delete', verifyAuth, async (req, res, next) => {
+router.delete('/delete', verifyAuth, async (req, res) => {
     const user_email = req.user.email;
+    const { id } = req.body;
 
-})
+    try {
+        const user = await db('users').select('*').where('email', user_email).first();
+
+        if (!user) {
+            return res.status(400).send({ message: 'there is no such a user' });
+        }
+
+
+        if (user.user_type !== 'user') {
+            return res.status(400).send({ message: ' only users can delete porduct to card' });
+        }
+
+
+        await db('cart').select('*').where('product_ids', id).del();
+
+        return res.status(200).send({
+            message: 'The product deleted to the cart successfully'
+        }
+
+        );
+
+    } catch (error) {
+        console.error('Hata:', error);
+        return res.status(400).send({ message: 'Something wrong brother.' });
+    }
+});
+
+
+router.get('/list/:cartId', verifyAuth, async (req, res) => {
+    const user_email = req.user.email;
+    const { cartId } = req.params;
+
+    try {
+        const user = await db('users').select('*').where('email', user_email).first();
+
+        if (!user) {
+            return res.status(400).send({ message: 'there is no such a user' });
+        }
+
+
+        if (user.user_type !== 'user') {
+            return res.status(400).send({ message: ' only users can list porduct with  given id' });
+        }
+
+
+        const cart = await db('cart').select('*').where('id', cartId).first();
+        if (!cart) {
+            res.status(400).send('There is no product such kind')
+        }
+
+        return res.status(200).send({
+            message: 'succesfull',
+            cart
+        });
+
+    } catch (error) {
+        console.error('Hata:', error);
+        return res.status(400).send({ message: 'Something wrong brother.' });
+    }
+});
+
+router.get('/:userId', verifyAuth, async (req, res) => { ///list/:userId bu durumda hata alıyorum???
+    const user_email = req.user.email;
+    const { userId } = req.params;
+
+    try {
+        const user = await db('users').select('*').where('email', user_email).first();
+
+        if (!user) {
+            return res.status(400).send({ message: 'there is no such a user' });
+        }
+
+
+        if (user.user_type !== 'user') {
+            return res.status(400).send({ message: ' only users can list porduct with  given id' });
+        }
+
+
+        const carts = await db('cart').innerJoin('users', 'users.id', 'cart.user_id').select('*').where('user_id', userId);
+        console.log('CARTS:', carts);
+        if (carts.length === 0) {
+            return res.status(400).send('This user hast any cart ');
+        }
+
+        return res.status(200).send({
+            message: 'succesfull',
+            carts
+        });
+
+    } catch (error) {
+        console.error('Hata:', error);
+        return res.status(400).send({ message: 'Something wrong brother.' });
+    }
+});
 
 
 module.exports = router;
-
