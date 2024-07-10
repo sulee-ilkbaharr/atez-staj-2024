884ee458576907ea18598e7b1835befa5c2ca121
Şule İlkbahar <suleilkbahar@Sule-MacBook-Air.local>
2024-07-10 20:39:29 +0300

    create cart is done function

--- routes/cart.js
+++ routes/cart.js
@@ -2,29 +2,45 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db/knex.db');
 const verifyAuth = require('./auth');
+const { route } = require('./products');
 
+/*user can create cart*/
 router.post('/add', verifyAuth, async function (req, res, next) {
     const user_email = req.user.email;
     console.log('user_email:', user_email);
 
+
+
     try {
+        //check auth
         const user = await db('users').select('*').where('email', user_email).first();
-        console.log('USER:', user);
         if (!user) {
             return res.status(400).send({
                 message: 'You should be authorize first.'
             });
         }
+
+        //chech user_type
         if (user.user_type !== 'user') {
             return res.status(400).send({
-                message: 'Only the users can add cart.'
+                message: 'Only the users can create cart and add produts to this cart.'
             })
         }
 
-        const cart = await db('cart').insert({ user_id: user.id });
+        // cheach if the user created cart befare
+        const cart = await db('cart').innerJoin('users', 'users.id', 'cart.user_id').select('*').where('email', user_email);
+        if (cart.length !== 0) {
+             return res.status(400).send({
+                message: 'user already has a  cart.',
+                cart
+            })
+        }
+
+        //a create cart
+        const newCart = await db('cart').insert({ user_id: user.id });
         return res.status(200).send({
             message: 'cart added succeffuly',
-            cart
+            newCart
         })
     } catch (error) {
         res.status(400).send({
@@ -34,6 +50,14 @@ router.post('/add', verifyAuth, async function (req, res, next) {
     }
 });
 
+
+
+router.post('/addproduct', verifyAuth,);
+
+router.delete('/delete', verifyAuth, async (req, res, next) => {
+    const user_email = req.user.email;
+
+})
 
 
 module.exports = router;
